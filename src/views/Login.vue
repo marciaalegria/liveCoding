0f8e39c912df3c5669ff308db879f4cd43622b83
marciaalegria <ytasegundo@gmail.com>
2020-12-11 23:09:01 -0300

    login

---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container >
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="Correo"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="pass"
+        :rules="passRules"
+        label="Contraseña"
+        type="password"
+        required
+      ></v-text-field>
+
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+        Validar
+      </v-btn>
+
+      <v-btn color="error" class="mr-4" @click="reset"> Limpiar</v-btn>
+    </v-form>
+  </v-container>
+</template>
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [(v) => !!v || "Name is required"],
+    email: "",
+    emailRules: [(v) => !!v || "E-mail is required"],
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+  },
+};
+</script>
